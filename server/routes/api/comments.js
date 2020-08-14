ca383d0ea2601843e6bc02cc91f6838a2601b792
DariusRain <dariusrain.student@careerdevs.com>
2020-08-13 20:15:08 -0400

    Added comments route

--- server/routes/api/comments.js
+++ server/routes/api/comments.js
@@ -35,3 +35,30 @@ router.post("/:postId", auth, async (req, res) => {
 // @route DELETE /api/comments
 // @desc Test route
 // @access RIVATE
+router.post("/:comentId", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        if (!post) {
+            res.status(400).json({msg: "Please create a profile before commenting."});
+        }
+        const index = post.comments.findIndex(comment => comment.id === req.params.commentId)
+        if (index === -1) {
+            res.status(404).json({msg: "Post not found"})
+        }
+        const profile = await Profile.findOne({user: req.user.id});
+
+        if(profile.id != post.comments) {
+
+        }
+        if (profile.id != post.comments[index].profile) {
+            res.status(401).json({msg: "Unauthorized."})
+        }
+        const comments = post.comments.splice(index, 1 );
+        await post.update({comments})
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({msg: "Server error."})
+    }
+    
+})
